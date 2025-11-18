116199794a828decfc709b480f68d35a4da1ecd6
the-destructor <73207042+the-destructor@users.noreply.github.com>
2025-11-18 13:34:14 +1100

    Update app.py

--- app.py
+++ app.py
@@ -87,6 +87,18 @@ def edit_movie(id):
 
     return render_template('edit.html', movie=movie)
 
+@app.route('/delete/<int:id>')
+
+def delete_movie(id):
+
+    movie = Movie.query.get_or_404(id)
+
+    db.session.delete(movie)
+
+    db.session.commit()
+
+    return redirect(url_for('index'))
+
 
 if __name__ == '__main__':
 
